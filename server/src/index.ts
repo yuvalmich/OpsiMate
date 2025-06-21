86a7c597d75f8a6a4f31a2775e1d06bc328e8df9
Yuval <user@users-MBP.lan>
2025-06-21 15:10:16 +0300

    implement routers

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,27 @@
 import express from 'express';
-import v1Router from './integration-router.ts';
+import integrationRouter from './integration-router';
+import healthRouter from './health-router';
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use('/api/v1', v1Router);
+app.use('/api/v1', integrationRouter);
+app.use('/', healthRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-}); 
+});
+
+/*
+public key
+ssh port
+username
+
+integrate with provider (VM, K8S, Aws ec2) -> service[]
+
+
+service_name
+service_ip
+service_status
+ */
